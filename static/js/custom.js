f82e1f33a54214a70d6f4d8bc70dd61f6a51b86d
Alexander Ivkin <ivkin@almworks.com>
2017-05-22 01:15:08 +0300

    Swap commenting feature desc and gif

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,11 @@
 $(function() {
-  $('.moqa-nav-container').width($('.moqa-header').width());
+  var width = $('.moqa-header').width();
+  $('.moqa-nav-container').width(width);
+  if (width < 500) {
+    var row$ = $('.moqa-feature-commenting');
+    var desc$ = row$.find('.moqa-feature-desc');
+    var gif$ = row$.find('.moqa-screen-gif-container');
+    gif$.remove();
+    gif$.appendTo(row$);
+  }
 });

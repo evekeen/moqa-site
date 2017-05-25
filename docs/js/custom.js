8ea0d875ad6d51f12cadb0cd90f162dcb43f4f18
Alexander Ivkin <ivkin@almworks.com>
2017-05-25 23:22:46 +0300

    Fixed centering and header margin

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -7,14 +7,17 @@ function updateSize() {
   var width = $(window).width();
   var contentWidth = $('.moqa-header').width();
   $('.moqa-nav-container').width(contentWidth);
+  var all$ = $('#all');
   var row$ = $('.moqa-feature-commenting');
   var gif$ = row$.find('.moqa-screen-gif-container-parent');
   var desc$ = row$.find('.moqa-feature-desc');
   if (width < 768) {
     gif$.remove();
     gif$.appendTo(row$);
+    all$.removeClass("moqa-big");
   } else {
     desc$.remove();
     desc$.appendTo(row$);
+    all$.addClass("moqa-big");
   }
 }
